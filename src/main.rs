193b17d946bd6fccccdc93aaad52b4560af7285f
AliothCancer <ortjuly7@gmail.com>
2025-02-20 13:02:08 +0100

    loggin ppt_value_fast changings

--- src/main.rs
+++ src/main.rs
@@ -10,105 +10,120 @@ use parsing_info::RyzenAdjInfo;
 fn main() {
     let target_fast = 20_000;
     let target_slow = 20_000;
-    let mut controller = Controller::new();
+    let mut controller = Controller::new(target_fast, target_slow);
 
     loop {
-        controller.action = State::Reading;
-        controller.populate_info();
-        let mut changes_iter = controller.changes.iter();
-        for change in changes_iter {
-            match change {
-                Changes::FastChanged(fast_limit) => {
-                    log!(
-                        Level::Warn,
-                        "FastLimit has changed from {} to {}",
-                        controller.fast.get_current_fast(),
-                        fast_limit
-                    );
-                    controller.action = State::Writing;
-                    controller.write_value(ToWriteValues::FastLimit(*fast_limit));
-                }
-                Changes::SlowChanged(slow_limit) => {
-                    log!(
-                        Level::Warn,
-                        "SlowLimit has changed from {} to {}",
-                        controller.fast.get_current_slow(),
-                        slow_limit
-                    );
-                    controller.write_value(ToWriteValues::SlowLimit(*slow_limit))
-                }
-            }
+        controller.update();
+        dbg!(&controller.changes);
+        if controller
+            .changes
+            .iter()
+            .any(|x| matches!(x, Changes::Fast(_) | Changes::Slow(_)))
+        {
+            controller.reset_limit();
         }
+        //if controller.changes.iter().any(|x| matches!(x, Changes::FastValue(_))) {
+        //    controller.reset_limit();
+        //}
+
         // DEBUG LINE
-        break;
+        //break;
+
+        // SLEEP TIME
         sleep(Duration::from_secs(1));
     }
 }
 
-enum RyzenAdjParam {
-    Fast { target: u32, current: u32 },
-    Slow { target: u32, current: u32 },
-}
-impl RyzenAdjParam {
-    fn get_current_fast(&self) -> u32 {
-        match self {
-            RyzenAdjParam::Fast { current, .. } => *current,
-            _ => panic!(),
-        }
-    }
-    fn get_current_slow(&self) -> u32 {
-        match self {
-            RyzenAdjParam::Slow { current, .. } => *current,
-            _ => panic!(),
-        }
-    }
-}
 struct Controller {
     action: State,
     fast_target: u32,
     fast_limit: u32,
+    value_fast: f32,
     slow_target: u32,
     slow_limit: u32,
     changes: Vec<Changes>,
 }
 impl Controller {
+    fn update(&mut self) {
+        /// push changes onto changes field
+        let ryzen_adj_info = match run_commands::get_info() {
+            Ok(info_output) => parsing_info::parse_ryzenadj_info(info_output),
+            Err(err) => panic!("PAnicking when getting ryzendadj info"),
+        };
+        self.changes.clear();
+        let RyzenAdjInfo {
+            stapm_value,
+            ppt_limit_fast,
+            ppt_value_fast,
+            ppt_limit_slow,
+            ppt_value_slow,
+            ppt_value_apu,
+        } = ryzen_adj_info;
+
+        // VALUES
+        if self.value_fast != ppt_value_fast {
+            //log!(Level::Warn,"Fast limit changed from {} to {}", self.fast_limit, ppt_limit_fast);
+            self.changes.push(Changes::FastValue(ppt_value_fast));
+        }
+
+        // LIMITS
+        if self.fast_limit != ppt_limit_fast as u32 {
+            log!(
+                Level::Warn,
+                "Fast limit changed from {} to {}",
+                self.fast_limit,
+                ppt_limit_fast
+            );
+            self.changes.push(Changes::Fast(ppt_limit_fast as u32));
+        }
+        if self.slow_limit != ppt_limit_slow as u32 {
+            log!(
+                Level::Warn,
+                "Slow limit changed from {} to {}",
+                self.slow_limit,
+                ppt_limit_slow
+            );
+            self.changes.push(Changes::Slow(ppt_limit_slow as u32));
+        }
+    }
     fn new(fast_target: u32, slow_target: u32) -> Self {
         let ryzen_adj_info = match run_commands::get_info() {
             Ok(info_output) => parsing_info::parse_ryzenadj_info(info_output),
             Err(err) => panic!("PAnicking when getting ryzendadj info"),
         };
 
-        match ryzen_adj_info {
-            RyzenAdjInfo {
-                stapm_value,
-                ppt_limit_fast,
-                ppt_value_fast,
-                ppt_limit_slow,
-                ppt_value_slow,
-                ppt_value_apu,
-            } => Self {
-                action: State::Nothing,
-                fast_target,
-                fast_limit: ppt_limit_fast as u32,
-                slow_target,
-                slow_limit: ppt_limit_slow as u32,
-                changes: vec![],
-            },
+        let RyzenAdjInfo {
+            stapm_value,
+            ppt_limit_fast,
+            ppt_value_fast,
+            ppt_limit_slow,
+            ppt_value_slow,
+            ppt_value_apu,
+        } = ryzen_adj_info;
+        Self {
+            action: State::Nothing,
+            fast_target,
+            fast_limit: ppt_limit_fast as u32,
+            value_fast: ppt_value_fast,
+            slow_target,
+            slow_limit: ppt_limit_slow as u32,
+            changes: vec![],
         }
     }
-    fn write_value(&self, to_write: ToWriteValues) {}
+
+    fn reset_limit(&self) {
+        run_commands::reset_fast_limit(self.fast_target);
+        run_commands::reset_slow_limit(self.slow_target);
+    }
 }
+#[derive(Debug, PartialEq, PartialOrd)]
 enum Changes {
-    FastChanged(u32),
-    SlowChanged(u32),
+    Fast(u32),
+    FastValue(f32),
+    Slow(u32),
 }
 enum State {
     Reading,
     Writing,
     Nothing,
-}
-
-enum ToWriteValues {
-    FastLimit(u32),
-    SlowLimit(u32),
 }
--- src/run_commands.rs
+++ src/run_commands.rs
@@ -1,16 +1,14 @@
-use std::process::Command;
-use std::io::{self, Write};
 use log::debug;
+use std::io::{self, Write};
+use std::process::Command;
 
 pub fn get_info() -> Result<String, io::Error> {
-    // Inizializza il logger
-    
-
     // Costruisci il comando
     let mut command = Command::new("sudo");
-    command.arg("/home/giulio/.local/bin/ryzenadj")
-    .arg("--info");
-    
+    command
+        .arg("/home/giulio/.local/bin/ryzenadj")
+        .arg("--info");
+
     //dbg!("Esecuzione del comando: {:?}", &command);
 
     // Esegui il comando e cattura l'output
@@ -27,5 +25,52 @@ pub fn get_info() -> Result<String, io::Error> {
         let stderr = String::from_utf8_lossy(&output.stderr).to_string();
         dbg!("Errore durante l'esecuzione del comando: {}", &stderr);
         Err(io::Error::new(io::ErrorKind::Other, stderr))
+    }
+}
+
+pub(crate) fn reset_fast_limit(target: u32) {
+    // Costruisci il comando
+    let mut command = Command::new("sudo");
+    command.arg("/home/giulio/.local/bin/ryzenadj")
+    .arg(format!("--fast-limit={}", target));
+    
+    //dbg!("Esecuzione del comando: {:?}", &command);
+
+    // Esegui il comando e cattura l'output
+    let output = command.output().unwrap();
+
+    // Verifica se il comando è stato eseguito con successo
+    if output.status.success() {
+        // Converte l'output in una stringa
+        let stdout = String::from_utf8_lossy(&output.stdout).to_string();
+        dbg!("Output del comando: {}", &stdout);
+        
+    } else {
+        // Se c'è stato un errore, cattura l'errore
+        let stderr = String::from_utf8_lossy(&output.stderr).to_string();
+        dbg!("Errore durante l'esecuzione del comando: {}", &stderr);
+    }
+}
+pub(crate) fn reset_slow_limit(target: u32) {
+    // Costruisci il comando
+    let mut command = Command::new("sudo");
+    command.arg("/home/giulio/.local/bin/ryzenadj")
+    .arg(format!("--slow-limit={}", target));
+    
+    //dbg!("Esecuzione del comando: {:?}", &command);
+
+    // Esegui il comando e cattura l'output
+    let output = command.output().unwrap();
+
+    // Verifica se il comando è stato eseguito con successo
+    if output.status.success() {
+        // Converte l'output in una stringa
+        let stdout = String::from_utf8_lossy(&output.stdout).to_string();
+        dbg!("Output del comando: {}", &stdout);
+        
+    } else {
+        // Se c'è stato un errore, cattura l'errore
+        let stderr = String::from_utf8_lossy(&output.stderr).to_string();
+        dbg!("Errore durante l'esecuzione del comando: {}", &stderr);
     }
 }
